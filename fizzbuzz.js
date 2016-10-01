5bacefbe5e5ef315806e115d2fd5bc460013cab6
nhodge253 <nhodge253@gmail.com>
2016-10-01 03:20:27 -0400

    Finished FizzBuzz assignment

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,22 @@
-// Place your FizzBuzz code here. 
+function fizzbuzz (){
+
+for(var i = 1; i <= 20; i++) {
+  
+  if (i % 3 == 0 && i % 5 == 0){
+    console.log ('FizzBuzz');
+   }
+  else if (i % 3 == 0) {
+    console.log ('Fizz');
+  }
+  else if (i % 5 == 0) {
+    console.log ('Buzz');
+  }
+  
+  else {
+   console.log(i);
+}
+  
+}// Place your FizzBuzz code here. 
+}
+
+fizzbuzz ();
