ee5dfd90b4deb19e3381e69127ce12ee35c7e4be
nhodge253 <nhodge253@gmail.com>
2016-10-01 03:47:01 -0400

    added another equal signs to each expression

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,15 +4,15 @@ function fizzbuzz (){
 //Count from 1-20 and stop at 20.
 for(var i = 1; i <= 20; i++) {
 //If the number is divisible by 3 & 5, call out 'FizzBuzz'.
-  if (i % 3 == 0 && i % 5 == 0){
+  if (i % 3 === 0 && i % 5 === 0){
     console.log ('FizzBuzz');
    }
 //If the number is only divisible by 3, call out 'Fizz'.
-  else if (i % 3 == 0) {
+  else if (i % 3 === 0) {
     console.log ('Fizz');
   }
 //If the number is only divisible by 5, call out 'Buzz'.
-  else if (i % 5 == 0) {
+  else if (i % 5 === 0) {
     console.log ('Buzz');
   }
 //If the number is not divisible by 3 or 5, just call out the number.
